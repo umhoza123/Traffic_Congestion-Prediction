4a1e1885b5fd3ed13510d488e641b865cbb0a6d9
umhoza123 <umhozarache123@gmail.com>
2025-10-18 22:08:48 +0200

    Add files via upload

---
+++ appp.py
@@ -0,0 +1,36 @@
+import pickle
+import numpy as np
+import streamlit as st
+with open(r'C:\Users\Rachel\Desktop\data set\ving.pkl', 'rb') as f:
+    model=pickle.load(f)
+st.title("traffic congestion")
+st.write("fill the following information") 
+week=st.selectbox("Day of week",['Mon','Tue','Wed','Thur','Fri','Sat','Sun'])
+CarCount=st.number_input("CarCount")
+BikeCount=st.number_input("BikeCount")
+BusCount=st.number_input("BusCount")
+Total=st.number_input("Total")
+
+if week=='Mon':
+    days=1
+if week=='Tue':
+    days=2
+if week=='wed':
+    days=3
+if week=='Thur':
+    days=4
+if week=='fri':
+    days=5
+if week=='sat':
+    days=6
+if week=='sun':
+    days=7
+
+
+    
+        
+
+if st.button("predict"):
+    data=np.array([[CarCount,BikeCount,BusCount,Total,days]])
+    pred=model.predict(data)
+    st.success(f"traffic congestion:{pred[0]}")
